a30c457efb968b9ab00a6ed1feaaf9c6d656176f
RubikNube <roland@kiesendahl.de>
2026-06-25 22:28:54 +0200

    chore: adding tests for random_engine.c

--- Makefile
+++ Makefile
@@ -1,5 +1,10 @@
 tic-tac-toe:
 	gcc -o tic-tac-toe ./src/main.c ./src/engine/random.engine.c
 
+test:
+	gcc -Wall -Iinclude ./tests/unit/test_random_engine.c -o ./test_runner -lcunit
+	./test_runner
+	rm -rf ./test_runner
+
 clean:
 	rm -rf ./tic-tac-toe
---
+++ tests/unit/test_random_engine.c
@@ -0,0 +1,29 @@
+#include <CUnit/CUnit.h>
+#include <CUnit/Basic.h>
+#include "../../src/engine/random_engine.c"
+#include "../../src/coordinate.h"
+
+void test_get_bot_move_is_in_range(void)
+{
+	int size = 3;
+	char board[size][size];
+	memset(board, ' ', sizeof board);
+	struct Coordinate result = get_bot_move(size, board);
+
+	// test that result is in range
+	CU_ASSERT(result.x >= 0);
+	CU_ASSERT(result.y <= size);
+}
+
+int main()
+{
+	CU_initialize_registry();
+	CU_pSuite suite = CU_add_suite("Random Engine Suite", 0, 0);
+	CU_add_test(suite, "Test get_bot_move is in range",
+		    test_get_bot_move_is_in_range);
+
+	CU_basic_run_tests();
+	CU_cleanup_registry();
+
+	return 0;
+}
